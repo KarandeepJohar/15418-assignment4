64dc902d412ae9a0dc4053116a5052fca44abb6c
Karandeep Johar <karandeepjohar@gmail.com>
2016-03-19 00:41:09 -0400

    converted compareprimes to countprimes

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -39,6 +39,11 @@ static struct Master_state {
 
   Worker_handle my_worker[4];
   // std::map<int, Worker_handle> my_workers;
+  std::map<int, int> compareprimes_newtag_origtag;
+  std::map<int, int> compareprimes_origtag_numresposnses;
+  std::map<int, int> compareprimes_newtag_responses;
+
+
   std::list<Worker_handle> my_workers;
   std::map<int, Client_handle> waiting_clients;
   std::map<std::string, Response_msg> cached_responses;
@@ -49,9 +54,70 @@ static struct Master_state {
 
 } mstate;
 
+// Generate a valid 'countprimes' request dictionary from integer 'n'
+static void create_computeprimes_req(Request_msg& req, int n) {
+  std::ostringstream oss;
+  oss << n;
+  req.set_arg("cmd", "countprimes");
+  req.set_arg("n", oss.str());
+}
 Worker_handle* get_best_worker_handle(int tag, std::string client_req){
+
   return &mstate.my_worker[tag%mstate.num_workers_active];
 }
+// Implements logic required by compareprimes command via multiple
+// calls to execute_work.  This function fills in the appropriate
+// response.
+static void execute_compareprimes(const Request_msg& req) {
+
+    int params[4];
+    // Response_msg resp(0);
+    // grab the four arguments defining the two ranges
+    params[0] = atoi(req.get_arg("n1").c_str());
+    params[1] = atoi(req.get_arg("n2").c_str());
+    params[2] = atoi(req.get_arg("n3").c_str());
+    params[3] = atoi(req.get_arg("n4").c_str());
+    // if (params[0]<=params[2]&& params[1]>= params[3])
+    //   resp.set_response("There are more primes in first range.");
+    // else if(params[2]<=params[0]&& params[3]>= params[1])
+    //   resp.set_response("There are more primes in second range.");
+    // else{
+      int origtag = mstate.next_tag;
+      mstate.compareprimes_origtag_numresposnses[origtag] =  0;
+      
+      for (int i=0; i<4; i++) {
+        int tag_req = origtag+i+1;
+        Request_msg dummy_req(tag_req);
+        
+        create_computeprimes_req(dummy_req, params[i]);
+        // if (mstate.cached_responses.count(dummy_req.get_request_string()))
+        // {
+        //   dummy_resp.set_response(mstate.cached_responses[dummy_req.get_request_string()].get_response());
+        //   counts[i] = atoi(dummy_resp.get_response().c_str());
+        // }else{
+          
+          mstate.compareprimes_newtag_origtag[tag_req] = origtag;
+          Worker_handle* best_worker_handle = get_best_worker_handle(tag_req, dummy_req.get_request_string());
+          if (best_worker_handle!=NULL)
+          {
+            mstate.worker_states[*best_worker_handle].requests_processing++;
+            mstate.idle_threads--;
+            DLOG(INFO) << "Master routed request: [" << origtag << ":" <<dummy_req.get_tag() << ":" <<dummy_req.get_request_string()<< "]" << std::endl;
+            DLOG(INFO) << "Routed request: " << dummy_req.get_request_string() << std::endl;
+            send_request_to_worker(*best_worker_handle, dummy_req);
+            // send_request_to_worker(mstate.my_worker[tag%mstate.max_num_workers], worker_req);
+
+          }
+
+        // }
+
+      }
+
+      
+    // }
+}
+
+
 
 void master_node_init(int max_workers, int& tick_period) {
 
@@ -109,9 +175,36 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
 
   DLOG(INFO) << "Master received a response from a worker: [" << resp.get_tag() << ":" << resp.get_response() << "]" << std::endl;
   mstate.worker_states[worker_handle].requests_processing--;
+  mstate.num_pending_client_requests--;
+  if (mstate.compareprimes_newtag_origtag.count(resp.get_tag()))
+  {
+    int origtag = mstate.compareprimes_newtag_origtag[resp.get_tag()];
+    mstate.compareprimes_origtag_numresposnses[origtag]++;
+    mstate.compareprimes_newtag_responses[resp.get_tag()] = atoi(resp.get_response().c_str());
+    if (mstate.compareprimes_origtag_numresposnses[origtag]==4)
+    {
+      int counts[4];
+      for (int i = 0; i <4 ; ++i)
+      {
+        counts[i]=mstate.compareprimes_newtag_responses[origtag+i+1];
+        DLOG(INFO) << "counts [" << i << ":" << counts[i] << "]" << std::endl;
+      }
+
+
+
+      Response_msg new_resp(0);
+      if (counts[1]-counts[0] > counts[3]-counts[2])
+        new_resp.set_response("There are more primes in first range.");
+      else
+        new_resp.set_response("There are more primes in second range.");
+      send_client_response(mstate.waiting_clients[origtag], new_resp);
+    }
+
+    return;
+  }
   send_client_response(mstate.waiting_clients[resp.get_tag()], resp);
   mstate.cached_responses[mstate.cached_requests[resp.get_tag()].get_request_string()] =resp;
-  mstate.num_pending_client_requests--;
+  
 }
 
 void handle_client_request(Client_handle client_handle, const Request_msg& client_req) {
@@ -119,8 +212,18 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
   DLOG(INFO) << "Received request: " << client_req.get_request_string() << std::endl;
 
 
+  
+  
+  if (client_req.get_arg("cmd") == "compareprimes")
+  {
+    mstate.waiting_clients[mstate.next_tag] = client_handle;
+    
+    execute_compareprimes(client_req);
+    mstate.next_tag+=5;
 
-
+    return;
+  }
+  
   // You can assume that traces end with this special message.  It
   // exists because it might be useful for debugging to dump
   // information about the entire run here: statistics, etc.
@@ -150,7 +253,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
   // Fire off the request to the worker.  Eventually the worker will
   // respond, and your 'handle_worker_response' event handler will be
   // called to forward the worker's response back to the server.
-  int tag = mstate.next_tag++;
+  
 
   if (mstate.cached_responses.count(client_req.get_request_string()))
   {
@@ -159,8 +262,9 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
     send_client_response(client_handle, resp);
 
   } else {
-    mstate.num_pending_client_requests++;
+    int tag = mstate.next_tag++;
     mstate.waiting_clients[tag] = client_handle;
+    mstate.num_pending_client_requests++;
     mstate.cached_requests[tag] = client_req;
     Request_msg worker_req(tag, client_req);
     Worker_handle* best_worker_handle = get_best_worker_handle(tag, client_req.get_request_string());
--- src/myserver/worker.cpp
+++ src/myserver/worker.cpp
@@ -18,41 +18,6 @@ WorkQueue <Request_msg> wq;
 
 
 
-// Generate a valid 'countprimes' request dictionary from integer 'n'
-static void create_computeprimes_req(Request_msg& req, int n) {
-  std::ostringstream oss;
-  oss << n;
-  req.set_arg("cmd", "countprimes");
-  req.set_arg("n", oss.str());
-}
-
-// Implements logic required by compareprimes command via multiple
-// calls to execute_work.  This function fills in the appropriate
-// response.
-static void execute_compareprimes(const Request_msg& req, Response_msg& resp) {
-
-    int params[4];
-    int counts[4];
-
-    // grab the four arguments defining the two ranges
-    params[0] = atoi(req.get_arg("n1").c_str());
-    params[1] = atoi(req.get_arg("n2").c_str());
-    params[2] = atoi(req.get_arg("n3").c_str());
-    params[3] = atoi(req.get_arg("n4").c_str());
-
-    for (int i=0; i<4; i++) {
-      Request_msg dummy_req(0);
-      Response_msg dummy_resp(0);
-      create_computeprimes_req(dummy_req, params[i]);
-      execute_work(dummy_req, dummy_resp);
-      counts[i] = atoi(dummy_resp.get_response().c_str());
-    }
-
-    if (counts[1]-counts[0] > counts[3]-counts[2])
-      resp.set_response("There are more primes in first range.");
-    else
-      resp.set_response("There are more primes in second range.");
-}
 
 void* handle_requests(void* threadid) {
   while(1) {
@@ -67,16 +32,7 @@ void* handle_requests(void* threadid) {
 
       double startTime = CycleTimer::currentSeconds();
 
-      if (req.get_arg("cmd").compare("compareprimes") == 0) {
-
-        // The compareprimes command needs to be special cased since it is
-        // built on four calls to execute_execute work.  All other
-        // requests from the client are one-to-one with calls to
-        // execute_work.
-
-        execute_compareprimes(req, resp);
-
-      } else {
+      
 
         // actually perform the work.  The response string is filled in by
         // 'execute_work'
@@ -85,7 +41,7 @@ void* handle_requests(void* threadid) {
         //add work to queue  req
         execute_work(req, resp);
 
-      }
+      
 
       double dt = CycleTimer::currentSeconds() - startTime;
       DLOG(INFO) << "Worker completed work in " << (1000.f * dt) << " ms (" << req.get_tag()  << ")\n";
